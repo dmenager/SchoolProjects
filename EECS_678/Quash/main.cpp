c3f72fc83bc9aea10202b2992971ef5fb0c45f91
dmenager <dhmenager@gmail.com>
2014-03-16 03:12:36 -0500

    Parse multiple commands. Set up child processes. Child processes aren't running new commands yet but work for testing. See TODO's in main.cpp

--- EECS_678/Quash/main.cpp
+++ EECS_678/Quash/main.cpp
@@ -7,12 +7,19 @@
 #include <exception>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 #define BUF_SIZE 256
 
 using namespace std;
 
-void PathSearch(string path);
+
+typedef struct
+{
+  int fd[2];
+} FileDescriptors;
+
+void PathSearch(queue<string>& q, string path);
 void tokenize(string str, queue<string>& q, char* delim);
 /**Parse them args and jank. Start some processes 
  * argc = argument count
@@ -23,22 +30,67 @@ char* t_cmd;
 int main(int argc, char* argv[], char* envp[])
 {
   cout << "Welcome to QUASH!\n";
-  //wait for a string to be inputted;
+  //wait for a string to be submitted
   string workingDir, cmd;
   char buf[BUF_SIZE];
   
   while(cmd != "exit")
   {
+    //list of processes
+    vector<queue<string> > ps;
+
+    //list of file descriptors
+    vector<FileDescriptors> fds;
+    
     //print working directory
     getcwd(buf, BUF_SIZE);
 
     workingDir.assign(buf, buf +1);
     printf( "%s>", buf);
     
-    cin >>cmd;
-  
+    getline(cin, cmd);
+    queue<string> q_cmd;
+
+    tokenize(cmd, q_cmd, "|");
+    //TODO: parse redirections as well...not quite sure where to do this yet
+
+
+    int i = 0;
+    //for each item in q_cmd, make a new procress for it
+    while(!q_cmd.empty())
+    {
+      char testBuf[1024], *s = "This is a test!\n";
+      queue<string> q;
+      ps.push_back(q);
+     
+      //f[0] == read
+      //f[1] == write
+      FileDescriptors f;
+      pipe(f.fd);
+      tokenize(q_cmd.front(), ps.at(i), " ");
+      q_cmd.pop();
+      fds.push_back(f);
+      
+      //create and run new process
+      if(fork() == 0)
+      {
+        //TODO: close the unused read/write ends
+        //TODO: reroute stdin/out
+        write(fds[i].fd[1], s, strlen(s));
+        //execlpe(); 
+        exit(0);
+      }
+
+      read(fds[i].fd[0], testBuf, strlen(s)); 
+      printf(testBuf);
+      i++;
+    }
     
+    /*
+    //terminal arguments 
     queue<string> t_args;
+    
+    //paths
     queue<string> paths;
 
     tokenize(cmd, t_args, " |");
@@ -48,9 +100,9 @@ int main(int argc, char* argv[], char* envp[])
     try
     {
       //if this is not a full path search through the path variable
-      if(t_args.front()[0] != "/";
+      if(t_args.front()[0] != '/')
       {
-        PathSearch(paths, t_args.front());
+        PathSearch(t_args, paths);
       
         for(int i = 0; i < paths.size(); i++)
         {
@@ -79,6 +131,7 @@ int main(int argc, char* argv[], char* envp[])
     {
        printf(e.what());
     }
+   */
   }
   
   return 0;
